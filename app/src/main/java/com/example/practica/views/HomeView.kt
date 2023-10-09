13aad24e39636a9cb4da0061eda0e5b77e67b054
hipper <federicohipper@gmail.com>
2023-10-08 23:15:52 -0300

    agrego vista previa de elementos ya visualizados

--- app/src/main/java/com/example/practica/views/HomeView.kt
+++ app/src/main/java/com/example/practica/views/HomeView.kt
@@ -1,40 +1,120 @@
 package com.example.practica.views
 
 import android.content.Context
+import androidx.compose.foundation.layout.Arrangement
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 
 @Composable
 fun Home(context: Context, catalogoEsVisible: MutableState<Boolean>) {
+    val objetosVistosRecientemente = remember { mutableStateOf<List<String>>(emptyList()) }
+
+    LaunchedEffect(1) {
+        objetosVistosRecientemente.value = buscarObjetosVistosRecientemente(context)
+    }
+
     val addFileLauncher = rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) {
         if (it != null) {
             println("Selected file URI: $it")
         }
     }
-    Button(
-        onClick = {
-            addFileLauncher.launch("*/*")
-        },
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 10.dp, end = 10.dp)
+    Column(
+        modifier = Modifier,
+        verticalArrangement = Arrangement.Top
     ) {
-        Text(text= "Seleccionar nuevo")
+        Button(
+            onClick = {
+                addFileLauncher.launch("*/*")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp, 16.dp, 16.dp, 0.dp)
+        ) {
+            Text(text= "Seleccionar nuevo")
+        }
+        Button(
+            onClick = {catalogoEsVisible.value = true},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp, 8.dp, 16.dp, 0.dp)
+        ) {
+            Text(text = "Buscar en catálogo")
+        }
+        ListaObjetosRecientes(objetosVistosRecientemente.value)
     }
-    Button(
-        onClick = {catalogoEsVisible.value = true},
+}
+
+@Composable
+fun ListaObjetosRecientes(objetosVistosRecientemente: List<String>) {
+    Text(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 10.dp, end = 10.dp)
-    ) {
-        Text(text = "Buscar en catálogo")
+            .padding(16.dp, 50.dp, 0.dp, 0.dp),
+        text = "Objetos vistos recientemente"
+    )
+    Box (
+        modifier = Modifier
+            .padding(0.dp, 0.dp, 0.dp, 0.dp)
+            .height(400.dp)) {
+        LazyColumn(
+        ) {
+            objetosVistosRecientemente?.let {
+                items(it.size) { index ->
+                    ObjetoReciente(it[index])
+                }
+            }
+        }
     }
+}
+
+@Composable
+fun ObjetoReciente(nombreObjeto: String) {
+    Card(
+        colors = CardDefaults
+            .cardColors(containerColor = MaterialTheme.colorScheme.surfaceVariant),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp, 16.dp, 16.dp, 0.dp)
+    ) {
+        Text(
+            modifier = Modifier.padding(8.dp),
+            text = nombreObjeto
+        )
+    }
+}
+
+fun buscarObjetosVistosRecientemente(context: Context): List<String> {
+    val nombresDeArchivos = mutableListOf<String>()
+
+    val directorio = context.getExternalFilesDir(null)
+
+    if (directorio != null && directorio.exists() && directorio.isDirectory) {
+        val archivos = directorio.listFiles()
+        if (archivos != null) {
+            for (archivo in archivos) {
+                if (archivo.isFile) {
+                    nombresDeArchivos.add(archivo.name)
+                }
+            }
+        }
+    }
+    return nombresDeArchivos
 }
--- app/src/main/java/com/example/practica/views/Spinner.kt
+++ app/src/main/java/com/example/practica/views/Spinner.kt
@@ -1,14 +1,26 @@
 package com.example.practica.views
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+@Preview
 @Composable
 fun Spinner() {
-    CircularProgressIndicator(
-        modifier = Modifier.width(48.dp)
-    )
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier.width(48.dp)
+        )
+    }
 }
