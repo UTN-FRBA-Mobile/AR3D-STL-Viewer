3f0fc893ca00496fb50ef551a58bc670e5971430
fedehipper <federicohipper@gmail.com>
2023-10-30 12:47:31 -0300

    elimino Column de mas

--- app/src/main/java/com/example/practica/views/ConfirmacionStlView.kt
+++ app/src/main/java/com/example/practica/views/ConfirmacionStlView.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -22,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -56,15 +58,18 @@ fun ConfirmarStl(navController: NavHostController, textoTopBar: MutableState<Str
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.SpaceBetween
         ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
+        Card(
+            modifier = Modifier.fillMaxWidth()
         ) {
-            Card(
-                modifier = Modifier.fillMaxWidth()
-            ) {
+            Row {
                 Text(
-                    text = "Seleccionaste el archivo ${fileName.value}.stl",
-                    Modifier.padding(16.dp)
+                    text = "Seleccionaste el archivo",
+                    Modifier.padding(start = 16.dp, top= 16.dp, end = 0.dp, bottom = 16.dp)
+                )
+                Text(
+                    text = "${fileName.value}.stl",
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(start = 4.dp, 16.dp)
                 )
             }
         }
