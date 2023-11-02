d90522272a67f79674ceaec746d5c72bf34ce175
fedehipper <federicohipper@gmail.com>
2023-11-01 21:20:08 -0300

    agrego spinner al intentar buscar pagina siguiente

--- app/src/main/java/com/example/practica/views/CatalogoView.kt
+++ app/src/main/java/com/example/practica/views/CatalogoView.kt
@@ -15,10 +15,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -93,6 +95,14 @@ fun Catalogo(navController: NavHostController, textoTopBar: MutableState<String>
                     if (item != null) {
                         CardObjeto3d(context, item, busquedaArchivoStlViewModel)
                     }
+                }
+                item {
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                            .wrapContentSize(Alignment.Center)
+                    )
                 }
             }
         }
